6d225570a7998c6b1d9795ffc4ef64a68cec71c1
エンジニアリング部 <@LAPTOP-QCNIIJG5.nkano1.kt.home.ne.jp>
2025-09-04 18:03:02 +0900

    2025/9/4 鈴木

---
+++ src/main/java/com/example/moattravel/MoattravelApplication.java
@@ -0,0 +1,13 @@
+package com.example.moattravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoattravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoattravelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/moattravel/MoattravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moattravel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoattravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
