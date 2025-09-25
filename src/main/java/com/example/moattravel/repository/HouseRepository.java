602f206fbda7e81aa8f06cff447cc92990569c29
エンジニアリング部 <@LAPTOP-QCNIIJG5.nkano1.kt.home.ne.jp>
2025-09-25 15:13:44 +0900

    2025/9/25/15;00

--- src/main/java/com/example/moattravel/repository/HouseRepository.java
+++ src/main/java/com/example/moattravel/repository/HouseRepository.java
@@ -6,7 +6,32 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.moattravel.entity.House;
 
-public interface HouseRepository extends JpaRepository<House, Integer>{
-	public Page<House> findByNameLike(String keyword, Pageable pageable);
-}
+public interface HouseRepository extends JpaRepository<House, Integer> {
 
+	public Page<House> findByNameLike(String keyword, Pageable pageable);
+
+	public Page<House> findByNameLikeOrAddressLike(String nameKeyword, String addressKeyword, Pageable pageable);
+
+	public Page<House> findByAddressLike(String area, Pageable pageable);
+
+	public Page<House> findByPriceLessThanEqual(Integer price, Pageable pageable);
+
+	public Page<House> findByNameLikeOrAddressLikeOrderByCreatedAtDesc(String nameKeyword, String addressKeyword,
+			Pageable pageable);
+
+	public Page<House> findByNameLikeOrAddressLikeOrderByPriceAsc(String nameKeyword, String addressKeyword,
+			Pageable pageable);
+
+	public Page<House> findByAddressLikeOrderByCreatedAtDesc(String area, Pageable pageable);
+
+	public Page<House> findByAddressLikeOrderByPriceAsc(String area, Pageable pageable);
+
+	public Page<House> findByPriceLessThanEqualOrderByCreatedAtDesc(Integer price, Pageable pageable);
+
+	public Page<House> findByPriceLessThanEqualOrderByPriceAsc(Integer price, Pageable pageable);
+
+	public Page<House> findAllByOrderByCreatedAtDesc(Pageable pageable);
+
+	public Page<House> findAllByOrderByPriceAsc(Pageable pageable);
+
+}
--- src/main/java/com/example/moattravel/security/WebSecurityConfig.java
+++ src/main/java/com/example/moattravel/security/WebSecurityConfig.java
@@ -12,19 +12,19 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+
 public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-		http
-				.authorizeHttpRequests((requests) -> requests
-						.requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/").permitAll() // すべてのユーザーにアクセスを許可するURL   
-						.requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/", "/signup/**")
-						.permitAll() // すべてのユーザーにアクセスを許可するURL   
-						.requestMatchers("/admin/**").hasRole("ADMIN") // 管理者にのみアクセスを許可するURL
-						.anyRequest().authenticated() // 上記以外のURLはログインが必要（会員または管理者のどちらでもOK）
-				)
+        http
+            .authorizeHttpRequests((requests) -> requests                                
+              .requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/", "/signup/**").permitAll()  // すべてのユーザーにアクセスを許可するURL
+                 .requestMatchers("/css/**", "/images/**", "/js/**", "/storage/**", "/", "/signup/**", "/houses").permitAll()  // すべてのユーザーにアクセスを許可するURL
+                .requestMatchers("/admin/**").hasRole("ADMIN")  // 管理者にのみアクセスを許可するURL
+                .anyRequest().authenticated()                   // 上記以外のURLはログインが必要（会員または管理者のどちらでもOK）
+           )
 
 				.formLogin((form) -> form
 						.loginPage("/login") // ログインページのURL
