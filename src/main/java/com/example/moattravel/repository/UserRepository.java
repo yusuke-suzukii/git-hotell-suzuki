cac78f3cb866e2008bdcc457b20577220def36fb
エンジニアリング部 <@LAPTOP-QCNIIJG5.nkano1.kt.home.ne.jp>
2025-09-25 12:12:50 +0900

    2025/9/25/12:00

--- src/main/java/com/example/moattravel/repository/UserRepository.java
+++ src/main/java/com/example/moattravel/repository/UserRepository.java
@@ -1,10 +1,13 @@
 package com.example.moattravel.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.moattravel.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {	
 public User findByEmail(String email);
+public Page<User> findByNameLikeOrFuriganaLike(String nameKeyword, String furiganaKeyword, Pageable pageable);
 
 }
